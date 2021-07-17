d54e4182506b5c967c906b88b69013582cb330d6
muffin-bird <muffinmen617@gmail.com>
2021-07-17 22:00:34 +0900

    Create MyFirst

---
+++ MyFirst/MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by muffin man on 2021/07/17.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
