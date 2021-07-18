c76e5c42ee23926376e3ff4d951d8b7f0ec0d4c7
muffin-bird <muffinmen617@gmail.com>
2021-07-19 08:43:16 +0900

    Create Button

--- MyFirst/MyFirst/ContentView.swift
+++ MyFirst/MyFirst/ContentView.swift
@@ -8,15 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var outputText = "Hello, World!"
+    
     var body: some View {
-        Text("Hello, World!")
-            .font(.largeTitle)
-            .fontWeight(.regular)
-            .foregroundColor(Color.blue)
-            .padding()
+        
+        VStack{
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .fontWeight(.regular)
+                .foregroundColor(Color.blue)
+                .padding()
             
-    }
-}
+            Button(action: {
+                outputText = "Hi, Swift!"
+            }) {
+                Text("切り替えボタン")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding(.all)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+            }
+        } // VStack ここまで
+    } // body ここまで
+} // ContentView ここまで
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
