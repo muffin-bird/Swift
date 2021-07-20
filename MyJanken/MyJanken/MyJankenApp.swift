168d27e88342ec9d7c118ee83d099f76ddff1322
muffin-bird <muffinmen617@gmail.com>
2021-07-20 21:08:32 +0900

    Create MyJanken

---
+++ MyJanken/MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by muffin man on 2021/07/20.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
